ed726999cb206e4bb6a8a1a5e2c1868c31384b51
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-10-26 09:58:43 -0300

    Agrego archivo para force drop de las tablas del schema GESTIONATE

---
+++ borrar_tablas.sql
@@ -0,0 +1,17 @@
+--Borra las tablas del schema 'GESTIONATE'
+--Correr cuantas veces sea necesario (funciona medio como el culo)
+--El borrazo es a fuerza bruta, se deshabilitan todas las protecciones para hacerlo asi que
+--recomiendo no modificar.
+--																		Mateo.
+EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+
+DECLARE @sql NVARCHAR(max)=''
+
+SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
+FROM   INFORMATION_SCHEMA.TABLES
+WHERE  TABLE_TYPE = 'BASE TABLE'
+AND TABLE_SCHEMA = 'GESTIONATE'
+
+Exec Sp_executesql @sql
+
+EXEC sp_msforeachtable "ALTER TABLE ? CHECK CONSTRAINT all"
