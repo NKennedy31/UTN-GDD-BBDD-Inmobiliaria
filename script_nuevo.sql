5f0b4236e9b70191163aa06d4b3049be9e4c2def
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-10-29 23:00:59 -0300

    Mejora en los tiempos (15seg) por cambiar el numero_doc de VARCHAR(100) a numeric(18,0)

--- script_nuevo.sql
+++ script_nuevo.sql
@@ -1,29 +1,32 @@
 USE [GD2C2023]
 GO
+DECLARE @t AS DATETIME = GETDATE()
+
 
 -- !!!!!!!!!!!!!!!!!! --
 -- Creacion de TABLAS --
 -- ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ --
-
+PRINT('Comenzando migracion...')
+PRINT('Creando Tablas...')
 CREATE TABLE GESTIONATE.provincia(
 	id_provincia DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	nombre VARCHAR(100) NOT NULL,
 );
-GO
+
 
 CREATE TABLE GESTIONATE.localidad(
 	id_localidad DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	nombre VARCHAR(100) NOT NULL,
 	codigo_provincia DECIMAL(18,0) REFERENCES GESTIONATE.provincia,
 );
-GO
+
 
 CREATE TABLE GESTIONATE.barrio(
 	id_barrio DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1), 
 	nombre VARCHAR(100) NOT NULL,
 	codigo_localidad DECIMAL(18,0) REFERENCES GESTIONATE.localidad,
 );
-GO
+
 
 CREATE TABLE GESTIONATE.direccion(
 	id_direccion DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -32,7 +35,7 @@ CREATE TABLE GESTIONATE.direccion(
 	codigo_localidad DECIMAL(18,0) REFERENCES GESTIONATE.localidad,
 	codigo_provincia DECIMAL(18,0) REFERENCES GESTIONATE.provincia
 );
-GO
+
 
 CREATE TABLE GESTIONATE.sucursal(
 	id_sucursal DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -40,13 +43,13 @@ CREATE TABLE GESTIONATE.sucursal(
 	codigo_direccion DECIMAL(18,0) REFERENCES GESTIONATE.direccion,
 	telefono VARCHAR(100) NOT NULL,
 );
-GO
+
 
 CREATE TABLE GESTIONATE.agente(
 	id_agente DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	nombre VARCHAR(100) NOT NULL,
 	apellido VARCHAR(100) NOT NULL,
-	numero_doc VARCHAR(100) NOT NULL,
+	numero_doc numeric(18,0) NOT NULL,
 	tipo_doc CHAR(3) NOT NULL,
 	telefono VARCHAR(100) NOT NULL,
 	mail VARCHAR(100) NOT NULL,
@@ -54,50 +57,50 @@ CREATE TABLE GESTIONATE.agente(
 	fecha_registro DATETIME NOT NULL,
 	codigo_sucursal DECIMAL(18,0) REFERENCES GESTIONATE.sucursal
 );
-GO
+
 
 CREATE TABLE GESTIONATE.tipo_inmueble(
 	id_tipo_inmueble DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.ambiente(
 	id_ambiente DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.propietario(
 	id_propietario DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	nombre VARCHAR(100),
 	apellido VARCHAR(100),
-	numero_doc VARCHAR(100),
+	numero_doc numeric(18,0),
 	tipo_doc CHAR(3),
 	telefono VARCHAR(100),
 	mail VARCHAR(100),
 	fecha_nacimiento DATE,
 	fecha_registro DATETIME,
 );
-GO
+
 
 CREATE TABLE GESTIONATE.orientacion(
 	id_orientacion DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.disposicion(
 	id_disposicion DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.estado_inmueble(
 	id_estado_inmueble DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.inmueble(
 	id_inmueble DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -114,43 +117,43 @@ CREATE TABLE GESTIONATE.inmueble(
 	antiguedad DATE,
 	expensas NUMERIC(18,2)
 );
-GO
+
 
 CREATE TABLE GESTIONATE.caracteristica(
 	id_caracteristica DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100)
 );
-GO
+
 
 CREATE TABLE GESTIONATE.caracteristica_x_inmueble(
 	id_inmueble DECIMAL(18,0) REFERENCES GESTIONATE.inmueble,
 	id_caracteristica DECIMAL(18,0) REFERENCES GESTIONATE.caracteristica
 );
-GO
+
 
 CREATE TABLE GESTIONATE.moneda(
 	id_moneda DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.tipo_periodo(
 	id_tipo_periodo DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.tipo_operacion(
 	id_tipo_operacion DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.estado_anuncio(
 	id_estado_anuncio DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.anuncio(
 	id_anuncio DECIMAL(19,0) PRIMARY KEY IDENTITY(1,1),
@@ -165,26 +168,25 @@ CREATE TABLE GESTIONATE.anuncio(
 	fecha_finalizacion DATETIME NOT NULL,
 	costo_publicacion NUMERIC(18,2) NOT NULL
 );
-GO
 
 CREATE TABLE GESTIONATE.estado_alquiler(
 	id_estado_alquiler DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.inquilino(
 	id_inquilino DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	nombre VARCHAR(100),
 	apellido VARCHAR(100),
-	numero_doc VARCHAR(100),
+	numero_doc numeric(18,0),
 	tipo_doc CHAR(3),
 	telefono VARCHAR(50),
 	mail VARCHAR(100),
 	fecha_nacimiento DATE,
 	fecha_registro DATETIME
 );
-GO
+
 
 CREATE TABLE GESTIONATE.alquiler(
 	id_alquiler DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -202,26 +204,26 @@ CREATE TABLE GESTIONATE.alquiler(
 	id_tipo_periodo DECIMAL(18,0) REFERENCES GESTIONATE.tipo_periodo,
 	id_sucursal DECIMAL(18,0) REFERENCES GESTIONATE.sucursal
 );
-GO
+
 
 CREATE TABLE GESTIONATE.medio_de_pago(
 	id_medio_de_pago DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	detalle VARCHAR(100) NOT NULL
 );
-GO
+
 
 CREATE TABLE GESTIONATE.comprador(
 	id_comprador DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
 	nombre VARCHAR(100),
 	apellido VARCHAR(100),
-	numero_doc VARCHAR(100),
+	numero_doc numeric(18,0),
 	tipo_doc CHAR(3),
 	telefono VARCHAR(100),
 	mail VARCHAR(100),
 	fecha_nacimiento DATE,
 	fecha_registro DATETIME
 );
-GO
+
 
 CREATE TABLE GESTIONATE.venta(
     id_venta DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -233,7 +235,7 @@ CREATE TABLE GESTIONATE.venta(
     id_moneda DECIMAL(18,0) REFERENCES GESTIONATE.moneda,
     comision_inmobiliaria NUMERIC(18,2),
 );
-GO
+
 
 CREATE TABLE GESTIONATE.pago_venta(
 	id_pago DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -243,7 +245,6 @@ CREATE TABLE GESTIONATE.pago_venta(
 	id_moneda DECIMAL(18,0) REFERENCES GESTIONATE.moneda,
 	id_medio_de_pago DECIMAL(18,0) REFERENCES GESTIONATE.medio_de_pago
 );
-GO
 
 CREATE TABLE GESTIONATE.pago_alquiler(
     id_pago DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
@@ -257,7 +258,6 @@ CREATE TABLE GESTIONATE.pago_alquiler(
 	importe NUMERIC(18,2),
 	id_medio_de_pago DECIMAL(18,0) REFERENCES GESTIONATE.medio_de_pago
 );
-GO
 
 CREATE TABLE GESTIONATE.importe_periodo(
     id_alquiler DECIMAL(18,0) REFERENCES GESTIONATE.alquiler,
@@ -266,9 +266,10 @@ CREATE TABLE GESTIONATE.importe_periodo(
 	precio NUMERIC(18,2),
 	PRIMARY KEY(id_alquiler,periodo_inicio)
 );
+
+
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
 GO
-
-
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! --
 -- Creacion de FUNCIONES de utilidad para los procedures --
 -- ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ --
@@ -514,7 +515,7 @@ BEGIN
 
 	OPEN cursorCaracteristicas;
 	FETCH NEXT FROM cursorCaracteristicas INTO @inmueble_codigo, @cable, @gas, @wifi, @calefaccion;
-
+	BEGIN TRANSACTION t_caracteristica
 	WHILE @@FETCH_STATUS = 0
 	BEGIN		
 		IF @gas=1
@@ -536,7 +537,7 @@ BEGIN
 
 		FETCH NEXT FROM cursorCaracteristicas INTO @inmueble_codigo, @cable, @gas, @wifi, @calefaccion;
 	END
-
+	COMMIT TRANSACTION t_caracteristica
 	CLOSE cursorCaracteristicas;
 	DEALLOCATE cursorCaracteristicas;	
 END
@@ -715,17 +716,16 @@ BEGIN
 END
 GO
 
-
 -- !!!!!!!!!!!!!!!!!!! --
 -- Creacion de INDICES --
 -- ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ --
 
+DECLARE @t AS DATETIME = GETDATE()
+PRINT('Creando índices...')
 CREATE INDEX ind_provincia ON GESTIONATE.provincia (nombre);
 CREATE INDEX ind_localidad ON GESTIONATE.localidad (nombre);
 CREATE INDEX ind_barrio ON GESTIONATE.barrio (nombre);
-CREATE INDEX ind_inquilino ON GESTIONATE.inquilino (nombre);
-CREATE INDEX ind_agente ON GESTIONATE.agente (nombre);
-CREATE INDEX ind_propietario ON GESTIONATE.propietario (nombre);
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
 GO
 
 
@@ -733,34 +733,120 @@ GO
 -- EJECUCION de PROCEDURES --
 -- ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ --
 
-BEGIN TRANSACTION
+DECLARE @t AS DATETIME = GETDATE()
+PRINT('Migrando provincias...')
 EXEC GESTIONATE.migrar_provincia
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando localidades...')
 EXEC GESTIONATE.migrar_localidad
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando barrios...')
 EXEC GESTIONATE.migrar_barrio
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando direcciones...')
 EXEC GESTIONATE.migrar_direccion
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando sucursales...')
 EXEC GESTIONATE.migrar_sucursal
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando agentes...')
 EXEC GESTIONATE.migrar_agente
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando tipos de inmueble...')
 EXEC GESTIONATE.migrar_tipo_inmueble
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando ambientes...')
 EXEC GESTIONATE.migrar_ambiente
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando propietarios...')
 EXEC GESTIONATE.migrar_propietario
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando orientaciones...')
 EXEC GESTIONATE.migrar_orientacion
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando dispocisiones...')
 EXEC GESTIONATE.migrar_disposicion
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando estados de inmueble...')
 EXEC GESTIONATE.migrar_estado_inmueble
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando inmuebles...')
 EXEC GESTIONATE.migrar_inmueble
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando caracteristicas...')
 EXEC GESTIONATE.migrar_caracteristica
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando caracteristicas por inmueble...')
 EXEC GESTIONATE.migrar_caracteristica_x_inmueble
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando monedas...')
 EXEC GESTIONATE.migrar_moneda
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando tipos de periodo...')
 EXEC GESTIONATE.migrar_tipo_periodo
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando tipos de operacion...')
 EXEC GESTIONATE.migrar_tipo_operacion
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando estados de anuncio...')
 EXEC GESTIONATE.migrar_estado_anuncio
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando anuncios...')
 EXEC GESTIONATE.migrar_anuncio
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando estados de alquiler...')
 EXEC GESTIONATE.migrar_estado_alquiler
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando inquilinos...')
 EXEC GESTIONATE.migrar_inquilino
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando alquileres...')
 EXEC GESTIONATE.migrar_alquiler
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando medios de pago...')
 EXEC GESTIONATE.migrar_medio_de_pago
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando compradores...')
 EXEC GESTIONATE.migrar_comprador
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando ventas...')
 EXEC GESTIONATE.migrar_venta
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando pagos de venta...')
 EXEC GESTIONATE.migrar_pago_venta
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando pagos de alquiler...')
 EXEC GESTIONATE.migrar_pago_alquiler
+PRINT(CONCAT('Listo! ',DATEDIFF(millisecond,@t,GETDATE()),'ms'))
+SET @t = GETDATE()
+PRINT('Migrando importes de periodo...')
 EXEC GESTIONATE.migrar_importe_periodo
-COMMIT TRANSACTION
+
+GO
